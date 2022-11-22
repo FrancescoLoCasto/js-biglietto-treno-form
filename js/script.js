6dac44381a741c1f7885bf50c3fef5d044bec6dc
MyUsername <my@email.com>
2022-11-22 18:12:03 +0100

    continue create

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 let totalNumberOfKm = document.getElementById('totalKm')
 
 let ageSinglePassenger = document.getElementById('passengerAge')
+
+const ticketPriceForKm = 0.2762
+const shopButton  = document.getElementById('button-shop')
