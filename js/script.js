45a17758cebbe41c5de8255b3299c7b7a9e257da
MyUsername <my@email.com>
2022-11-22 18:24:21 +0100

    Creation of button and if, else row on js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@ let ageSinglePassenger = document.getElementById('passengerAge')
 
 const ticketPriceForKm = 0.2762
 const shopButton  = document.getElementById('button-shop')
+
+button.addEventListener('shop')
+
+   let shopPrice;
+   
+   totalTicketPrice = totalNumberOfKm.value * ticketPriceForKm;
+  
+
+
+   if (ageSinglePassenger.value < 19) {
+      totalTicketPrice = totalTicketPrice - (totalTicketPrice * 0.175);
+   } else if (ageSinglePassenger.value >= 65) {
+      totalTicketPrice = totalTicketPrice - (totalTicketPrice * 0.333);
+   }
